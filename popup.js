47f0a36614ff458b8e04b620d9bdb9bde82f8a33
CWY <cwyoo01@naver.com>
2021-07-15 23:27:22 +0900

    기본 UI 구현 완료

--- popup.js
+++ popup.js
@@ -1,3 +1,24 @@
+let finder = document.getElementById("findKoreaninComment");
+
+finder.style.backgroundColor = "silver";
+// finder에서 activate 버튼을 누르면 활성화되면 Green,
+// 비활성화되면 Default 색깔로 바뀌도록 해야 함.
+// 상태를 불러와서 Default 상태인지 Activate 상태인지 확인해서 색깔 설정해야 함.
+finder.addEventListener("click", async() => {
+    if (finder.style.backgroundColor === "lightgreen") {
+        finder.style.backgroundColor = "silver";
+        finder.value = "Activate!"
+    }
+    else {
+        finder.style.backgroundColor = "lightgreen";
+        finder.value = "Activated!"
+    }
+});
+
+/////////////////////////////////////////////////////////////////////////
+                    ////CHROME EXTENSION EXAMPLE////
+/////////////////////////////////////////////////////////////////////////
+/*
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
 
@@ -22,3 +43,4 @@ function setPageBackgroundColor() {
         document.body.style.backgroundColor = color;
     });
 }
+*/
